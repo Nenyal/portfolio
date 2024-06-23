6d76fdd48d88c97ffbfe43868f6d7b4cde63fcf9
Nenya <selimcankurtaran1406@gmail.com>
2024-06-23 19:54:16 +0300

    chore: fix specialities section for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -51,18 +51,18 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="flex flex-col px-1/4 items-center w-full gap-8 p-8 shadow-md rounded-lg">
-          <h2 className="text-2xl font-semibold">Experience</h2>
-          <div className="grid lg:grid-cols-3 grid-cols-1 gap-[34px] items-center hover">
-            <div className="flex flex-col items-center text-center gap-2 px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
+        <section className="flex flex-col lg:px-1/4 px-1/7 items-center w-full lg:gap-8 p-8 shadow-md rounded-lg">
+          <h2 className="text-2xl font-semibold">Specialities</h2>
+          <div className="grid lg:grid-cols-3 grid-cols-1 lg:gap-[34px] gap-[16px] items-center hover mt-8 lg:mt-0">
+            <div className="flex flex-col items-center text-center gap-2 lg:px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
               <Image src={js.src} alt="Javascript" width={50} height={50} />
-              <h2 className="text-xl font-bold text-[#3C3C3C]">JavaScript</h2>
+              <h2 className="text-lg font-bold text-[#3C3C3C]">JavaScript</h2>
               <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempo.
               </p>
             </div>
-            <div className="flex flex-col items-center text-center gap-2 px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
+            <div className="flex flex-col items-center text-center gap-2 lg:px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
               <Image src={csharp.src} alt="CSharp" width={50} height={50} />
               <h2 className="text-xl font-bold text-[#3C3C3C]">C#</h2>
               <p>
@@ -71,7 +71,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="flex flex-col items-center text-center gap-2 px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
+            <div className="flex flex-col items-center text-center gap-2 lg:px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
               <Image src={python.src} alt="Python" width={50} height={50} />
               <h2 className="text-xl font-bold text-[#3C3C3C]">Python</h2>
               <p>
@@ -80,7 +80,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="flex flex-col items-center text-center gap-2 px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
+            <div className="flex flex-col items-center text-center gap-2 lg:px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
               <Image src={java.src} alt="Java" width={50} height={50} />
               <h2 className="text-xl font-bold text-[#3C3C3C]">Java</h2>
               <p>
@@ -89,7 +89,7 @@ export default function Home() {
               </p>
             </div>
 
-            <div className="flex flex-col items-center text-center gap-2 px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
+            <div className="flex flex-col items-center text-center gap-2 lg:px-4 py-6 border-2 rounded-lg border-gray-50 transform transition duration-300 hover:scale-105">
               <Image
                 src={typescript.src}
                 alt="TypeScript"
