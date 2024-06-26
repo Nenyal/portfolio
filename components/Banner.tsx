3f0916f4f4dea2282894cf3fe10019c26c262f83
Nenya <selimcankurtaran1406@gmail.com>
2024-06-26 21:44:00 +0300

    fix: distance between two button in german

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -22,13 +22,13 @@ export default function Banner() {
           {t("description")}
         </p>
         <div className="flex flex-row items-center justify-center w-72 gap-4 mt-8">
-          <Link href="/contact" className="w-36 h-11 rounded-full">
+          <Link href="/contact" className="w-40 h-11 rounded-full">
             <Button className="w-fit h-11 text-white bg-darkblue-buttonbg hover:bg-buttonhover focus:outline-none focus:ring-2 focus:ring-blue-200 font-bold rounded-full text-base px-5 py-2.5 text-center me-2 mb-2">
               {t("contact")}
             </Button>
           </Link>
 
-          <Link href="/contact" className="w-36 h-11 rounded-full">
+          <Link href="/contact" className="w-40 h-11 rounded-full">
             <Button className="w-fit h-11 text-[#053C5F] bg-white ring-2 ring-[#053C5F] hover:bg-cyan-bg focus:outline-none focus:ring-2 focus:ring-blue-200 font-bold rounded-full text-base px-5 py-2.5 text-center me-2 mb-2">
               {t("more")}
             </Button>
