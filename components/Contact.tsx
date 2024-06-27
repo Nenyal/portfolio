5735432de3f832b7fbbf1f20e56da05ab1706f7f
Nenya <selimcankurtaran1406@gmail.com>
2024-06-27 22:16:30 +0300

    refactor: update Contact and About components with translations

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,11 +1,14 @@
+import { useTranslations } from "next-intl";
+
 export default function Contact() {
+  const t = useTranslations("Contact");
   return (
     <section
       id="contact"
       className="flex flex-col justify-center items-center gap-2 lg:gap-4 w-full px-1/6 p-16 lg:px-1/4 shadow-md rounded-lg"
     >
-      <h2 className="text-2xl font-semibold">Contact</h2>
-      <p className="mt-8">Follow me on:</p>
+      <h2 className="text-2xl font-semibold">{t("title")}</h2>
+      <p className="mt-8">{t("follow")}</p>
       <ul className="flex gap-4 mt-4">
         <li>
           <a
@@ -22,12 +25,11 @@ export default function Contact() {
           https://www.github.com/selimcankurtaran"
             className="text-black"
           >
-            
             GitHub
           </a>
         </li>
       </ul>
-      <p className="mt-8">Or send me an email:</p>
+      <p className="mt-8">{t("email")}</p>
       <a href="mailto:selimcankurtaran@hotmail.com" className="text-black">
         selimcankurtaran@hotmail.com
       </a>
